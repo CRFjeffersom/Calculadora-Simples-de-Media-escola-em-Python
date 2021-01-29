8f699c41fe32c2d6614f1d8d37a9a9f234fecbbc
CRFjeffersom <74440517+CRFjeffersom@users.noreply.github.com>
2021-01-29 20:35:53 -0300

    Add files via upload

---
+++ print ('').py
@@ -0,0 +1,8 @@
+print ('===== Calculadora de média =====') # Esse é um exemplo de print
+print ('Digite suas média abaixo')
+media1 = input(float('Sua primeira média aqui')) #Exemplo de entrada de dados
+media2 = input(float('Sua segunda média aqui')) 
+media3 = input(float('Sua terceira mèdia aqui')) 
+media4 = input(float('Sua Quarta média aqui')) 
+soma = media1 + media2 + media3 + media4 /4
+print ('soma')
